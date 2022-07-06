d5679399b340cef941258d89e13c65571a50cae2
Jerry <jerrycgh@gmail.com>
2022-07-05 17:32:05 -0700

    Create MVHashMap and use it StateDB

---
+++ core/state/mvhashmap.go
@@ -0,0 +1,180 @@
+package state
+
+import (
+	"encoding/base64"
+	"fmt"
+	"sync"
+
+	"github.com/emirpasic/gods/maps/treemap"
+)
+
+const FlagDone = 0
+const FlagEstimate = 1
+
+type MVHashMap struct {
+	rw sync.RWMutex
+	m  map[string]*TxnIndexCells // TODO: might want a more efficient key representation
+}
+
+func MakeMVHashMap() *MVHashMap {
+	return &MVHashMap{
+		rw: sync.RWMutex{},
+		m:  make(map[string]*TxnIndexCells),
+	}
+}
+
+type WriteCell struct {
+	flag        uint
+	incarnation int
+	data        *StateDB
+}
+
+type TxnIndexCells struct {
+	rw sync.RWMutex
+	tm *treemap.Map
+}
+
+type Version struct {
+	TxnIndex    int
+	Incarnation int
+}
+
+func (mv *MVHashMap) getKeyCells(k []byte, fNoKey func(kenc string) *TxnIndexCells) (cells *TxnIndexCells) {
+	kenc := base64.StdEncoding.EncodeToString(k)
+
+	var ok bool
+
+	mv.rw.RLock()
+	cells, ok = mv.m[kenc]
+	mv.rw.RUnlock()
+
+	if !ok {
+		cells = fNoKey(kenc)
+	}
+
+	return
+}
+
+func (mv *MVHashMap) Write(k []byte, v Version, data *StateDB) {
+	cells := mv.getKeyCells(k, func(kenc string) (cells *TxnIndexCells) {
+		n := &TxnIndexCells{
+			rw: sync.RWMutex{},
+			tm: treemap.NewWithIntComparator(),
+		}
+		var ok bool
+		mv.rw.Lock()
+		if cells, ok = mv.m[kenc]; !ok {
+			mv.m[kenc] = n
+			cells = n
+		}
+		mv.rw.Unlock()
+		return
+	})
+
+	// TODO: could probably have a scheme where this only generally requires a read lock since any given transaction transaction
+	//  should only have one incarnation executing at a time...
+	cells.rw.Lock()
+	defer cells.rw.Unlock()
+	ci, ok := cells.tm.Get(v.TxnIndex)
+
+	if ok {
+		if ci.(*WriteCell).incarnation > v.Incarnation {
+			panic(fmt.Errorf("existing transaction value does not have lower incarnation: %v, %v",
+				base64.StdEncoding.EncodeToString(k), v.TxnIndex))
+		} else if ci.(*WriteCell).flag == FlagEstimate {
+			println("marking previous estimate as done tx", v.TxnIndex, v.Incarnation)
+		}
+
+		ci.(*WriteCell).flag = FlagDone
+		ci.(*WriteCell).incarnation = v.Incarnation
+		ci.(*WriteCell).data = data
+	} else {
+		cells.tm.Put(v.TxnIndex, &WriteCell{
+			flag:        FlagDone,
+			incarnation: v.Incarnation,
+			data:        data,
+		})
+	}
+}
+
+func (mv *MVHashMap) MarkEstimate(k []byte, txIdx int) {
+	cells := mv.getKeyCells(k, func(_ string) *TxnIndexCells {
+		panic(fmt.Errorf("path must already exist"))
+	})
+
+	cells.rw.RLock()
+	if ci, ok := cells.tm.Get(txIdx); !ok {
+		panic("should not happen - cell should be present for path")
+	} else {
+		ci.(*WriteCell).flag = FlagEstimate
+	}
+	cells.rw.RUnlock()
+}
+
+func (mv *MVHashMap) Delete(k []byte, txIdx int) {
+	cells := mv.getKeyCells(k, func(_ string) *TxnIndexCells {
+		panic(fmt.Errorf("path must already exist"))
+	})
+
+	cells.rw.Lock()
+	defer cells.rw.Unlock()
+	cells.tm.Remove(txIdx)
+}
+
+const (
+	mvReadResultDone       = 0
+	mvReadResultDependency = 1
+	mvReadResultNone       = 2
+)
+
+type mvReadResult struct {
+	depIdx      int
+	incarnation int
+	value       *StateDB
+}
+
+func (mvr mvReadResult) status() int {
+	if mvr.depIdx != -1 {
+		if mvr.incarnation == -1 {
+			return mvReadResultDependency
+		} else {
+			return mvReadResultDone
+		}
+	}
+
+	return mvReadResultNone
+}
+
+func (mv *MVHashMap) Read(k []byte, txIdx int) (res mvReadResult) {
+	res.depIdx = -1
+	res.incarnation = -1
+
+	cells := mv.getKeyCells(k, func(_ string) *TxnIndexCells {
+		return nil
+	})
+	if cells == nil {
+		return
+	}
+
+	cells.rw.RLock()
+	defer cells.rw.RUnlock()
+
+	if fk, fv := cells.tm.Floor(txIdx - 1); fk != nil && fv != nil {
+		c := fv.(*WriteCell)
+		switch c.flag {
+		case FlagEstimate:
+			res.depIdx = fk.(int)
+			res.value = c.data
+		case FlagDone:
+			{
+				res.depIdx = fk.(int)
+				res.incarnation = c.incarnation
+				res.value = c.data
+			}
+		default:
+			panic(fmt.Errorf("should not happen - unknown flag value"))
+		}
+	}
+
+	return
+}
